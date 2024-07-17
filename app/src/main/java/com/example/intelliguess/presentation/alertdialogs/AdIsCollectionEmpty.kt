dbf68512f71b18f47ab711476f6ff4dbe217577c
brandon duran <duranbrandon927@gmail.com>
2024-07-17 15:00:15 +0800

    Update: Add spacer to CircularProgressIndicator

--- app/src/main/java/com/example/intelliguess/presentation/alertdialogs/AdIsCollectionEmpty.kt
+++ app/src/main/java/com/example/intelliguess/presentation/alertdialogs/AdIsCollectionEmpty.kt
@@ -4,7 +4,9 @@ import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowRight
@@ -52,7 +54,9 @@ fun IsCollectionEmpty(
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
+                    Spacer(Modifier.height(34.dp))
                     CircularProgressIndicator()
+                    Spacer(Modifier.height(34.dp))
                 }
             }
         )
